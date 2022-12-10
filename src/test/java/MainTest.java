cf36eef227b837ecabf1cdb29594a0f629d2b724
OliTheHoodieBoi <55047947+OliTheHoodieBoi@users.noreply.github.com>
2022-12-10 01:10:43 +0100

    Fixed incorrect usage of assertEquals

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest {
     @Test
     public void add1Test() {
-        assertEquals(Main.add1(1), 2);
+        assertEquals(2, Main.add1(1));
     }
 }
